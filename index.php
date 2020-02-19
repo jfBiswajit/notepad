1d14bae928e959f694dd32e83830efb39acd13e1
Biswajit Biswas <jfbiswajit@gmail.com>
2020-02-19 18:34:52 +0600

    Add rest button final

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +9,7 @@
   <link rel="stylesheet" href="asset/main.css">
   <title>Notepad</title>
 </head>
+
 <body>
   <main class="notepad">
     <textarea name="notepad" id="notepad" cols="120" rows="20" autofocus></textarea>
@@ -15,4 +17,5 @@
   </main>
   <script src="asset/app.js"></script>
 </body>
+
 </html>
